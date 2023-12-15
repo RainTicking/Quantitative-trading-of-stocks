38b6d85b58241e5cf750b5216a071ce2dfabbfe8
ganglee <ganglee@live.com>
2023-12-15 15:37:38 +0800

    modiry anay

--- data/stocks_analysis.sql
+++ data/stocks_analysis.sql
@@ -1,38 +1,38 @@
 
 -- 股票行情
 
-昨收 6
-今开 7
-今收 10
-涨幅% 199112
-涨跌 264648
-涨停 69
-跌停 70
-最高 8
-最低 9
-振幅% 526792
-成交量 13
-成交额 19
-换手% 1968584
-市盈率(动) 2034120
-市净率 1149395 / 592920
-总市值 3541450
-流通市值 3475914
-委比% 461256
-委差 395720
+昨收 6  yesterday closing price
+今开 7  today opening price
+今收 10 today closing price
+涨幅% 199112 rose percent
+涨跌 264648 rose price
+涨停 69 limit up
+跌停 70 limit down
+最高 8  high price
+最低 9  low price
+振幅% 526792 amplitude percent
+成交量 13 deal cnt
+成交额 19 deal amt 
+换手% 1968584 turnover rate
+市盈率(动) 2034120 P/E ratio (moving) PER
+市净率 1149395 / 592920 p/b ratio   market-to-book ratio  PBR
+总市值 3541450  market cap
+流通市值 3475914 float cap
+委比% 461256 appoint rate
+委差 395720  appoint diff
 
 买一价格 24
 买一委托量 25
 卖一价格 25
 卖一委托量 31
 
-外盘 14
-内盘 15
-总股本 402
-流通股 407
+外盘 14 outside dish
+内盘 15 inside dish
+总股本 402 total equity
+流通股 407 float stock
 
-盘后成交量 74
-均价 1378761
+盘后成交量 74 after hours deal cnt
+均价 1378761 avg price
 
 
 大单：
@@ -42,18 +42,20 @@
 小单：
 小于1万股或5万元的成交单。
 
-大单流入金额 223 + 225
-中单流入金额 259
-小单流入金额 237
+大单流入金额 223 + 225  big inflow amt
+中单流入金额 259    mid inflow amt
+小单流入金额 237    small inflow amt
 
-大单流出金额 224 + 226
-中单流出金额 260
-小单流出金额 238
+大单流出金额 224 + 226 big outflow amt
+中单流出金额 260    mid outflow amt
+小单流出金额 238    small outflow amt
 
-主力净额 大单流入金额 - 大单流出金额
-5日主力净额
-资金净流入 流入金额 - 流出金额
+主力净额 大单流入金额 - 大单流出金额 big net amt
+5日主力净额 5 day big net amt
+资金净流入 流入金额 - 流出金额  net inflow amt
 
 
-支撑位
-压力位
+
+支撑位  support level
+压力位  pressure level
+
